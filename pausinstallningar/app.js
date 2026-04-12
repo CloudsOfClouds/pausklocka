6a6bd889195d79cd777f60256374c00133fb51a7
CloudsOfClouds <200641924+CloudsOfClouds@users.noreply.github.com>
2026-04-12 17:36:56 +0200

    app.js

--- pausinstallningar/app.js
+++ pausinstallningar/app.js
@@ -1,1 +1,26 @@
-console.log("Sida 2 fungerar");
+let billboard = 18;
+let interview = 0;
+
+const billboardEl = document.getElementById("billboard");
+const interviewEl = document.getElementById("interview");
+
+function updateUI() {
+  billboardEl.textContent = billboard;
+  interviewEl.textContent = interview;
+}
+
+document.getElementById("setPreset").addEventListener("click", () => {
+  billboard = 18;
+  interview = 120;
+  updateUI();
+});
+
+document.getElementById("save").addEventListener("click", () => {
+  localStorage.setItem("billboard_duration", billboard);
+  localStorage.setItem("interview_home", interview);
+  localStorage.setItem("interview_away", 0);
+
+  alert("Sparat!");
+});
+
+updateUI();
